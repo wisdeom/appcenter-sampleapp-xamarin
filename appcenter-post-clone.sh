30da2b2a2437d2d43715abcebe912f2f4ec1a5df
Jihye Eom <37167493+wisdeom@users.noreply.github.com>
2018-10-18 17:41:58 +0900

    mv command added

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,5 +2,4 @@
 #
 
 echo "Project Name=$APPCENTER_XAMARIN_PROJECT"
-
-# mv $APPCENTER_SOURCE_DIRECTORY/appcenter-sampleapp-xamarin/iOS/Assets.xcassets $APPCENTER_SOURCE_DIRECTORY/appcenter-sampleapp-xamarin/iOS/Assets.xcassets.BAK
+mv $APPCENTER_SOURCE_DIRECTORY/appcenter-sampleapp-xamarin/iOS/Assets.xcassets $APPCENTER_SOURCE_DIRECTORY/appcenter-sampleapp-xamarin/iOS/Assets.xcassets.BAK
